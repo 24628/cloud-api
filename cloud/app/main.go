a3b1a0169e02410e2680a0d4613fb6548fa4f128
Bastiaan van der Bijl <666669@student.inholland.nl>
2023-09-11 23:58:35 +0200

    removed Merlijns dirty code

--- cloud/app/main.go
+++ cloud/app/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
+	})
+
+	r.Run(":8081")
 }
